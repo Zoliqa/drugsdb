62846220e2151393611d8dfa5c870d3187ab4499
Zoliqa <a_zzoli@yahoo.com>
2016-03-20 20:52:42 +0200

    added pig support

--- app/data.import/process.files.js
+++ app/data.import/process.files.js
@@ -1,35 +1,43 @@
 "use strict";
 
-const fs	     = require("fs"),
-	  q	         = require("q"),
-	  AdmZip     = require("adm-zip"),
-	  dom        = require("xmldom").DOMParser,
-	  xpath    	 = require("xpath"),
-	  parseXml   = require("./drug.xml.parser.js"),
-	  connection = require("../db/connection"),
-	  dirname    = process.argv[2];
+const fs	          = require("fs"),
+	  q	         	  = require("q"),
+	  AdmZip          = require("adm-zip"),
+	  dom        	  = require("xmldom").DOMParser,
+	  xpath    	 	  = require("xpath"),
+	  parseXml   	  = require("./drug.xml.parser.js"),
+	  connection  	  = require("../db/connection"),
+	  Drug       	  = require("../db/drug.model.js"),
+	  Substance  	  = require("../db/substance.model.js"),
+	  SubstanceDrugs  = require("../db/substance.drugs.model.js"),
+	  dirname    	  = process.argv[2];
 
 let promises = [];
 
-fs.readdir(dirname, (err, files) => {
-	files.forEach(file => {
-		if (file.endsWith(".zip")) {
-			let zip = new AdmZip(dirname + "/" + file);
+Drug.remove({})
+.then(() => Substance.remove({}))
+.then(() => SubstanceDrugs.remove({}))
+.then(() => {
+	fs.readdir(dirname, (err, files) => {
+		files.forEach(file => {
+			if (file.endsWith(".zip")) {
+				let zip = new AdmZip(dirname + "/" + file);
 
-			zip.getEntries().forEach(zipEntry => {
-				if (zipEntry.entryName.endsWith(".xml")) {
-					let xml = zip.readAsText(zipEntry.entryName);
-					let promise = parseXml(xml, zipEntry.entryName);
+				zip.getEntries().forEach(zipEntry => {
+					if (zipEntry.entryName.endsWith(".xml")) {
+						let xml = zip.readAsText(zipEntry.entryName);
+						let promise = parseXml(xml, zipEntry.entryName);
 
-					promises.push(promise);
+						promises.push(promise);
 
-					// zip.readAsTextAsync(zipEntry, xml => {
-					// 	parseXml(xml);
-					// });
-				}
-			});
-		}
+						// zip.readAsTextAsync(zipEntry, xml => {
+						// 	parseXml(xml);
+						// });
+					}
+				});
+			}
+		});
+
+		q.all(promises).then(() => process.send({ success: true }), () => process.send({ success: false }));
 	});
-
-	q.all(promises).then(() => process.send({ success: true }), () => process.send({ success: false }));
 });
--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -1,26 +1,37 @@
 "use strict";
 
-const express			= require("express"),
-	  child_process     = require('child_process'),
-	  passportUtilities = require("../passport/passport.utilities"),
-	  router			= express.Router();
+const express						= require("express"),
+	  child_process     			= require('child_process'),
+	  passportUtilities             = require("../passport/passport.utilities"),
+	  router			            = express.Router(),
+	  process_files_jsPath          = process.cwd() + "/data.import/process.files.js",
+  	  drugsInputDir                 = process.cwd() + "/../input/all/dm_spl_monthly_update_jan2016/homeopathic",
+	  join_substances_drugs_pigPath = process.cwd() + "/data.import/pig.queries/join_substances_drugs.pig",
+  	  pigPath                       = "/home/zoliqa/Downloads/pig/pig-0.15.0/bin/pig";
 
 router.post("/import", passportUtilities.isAdminUser, function (req, res, next) {
-	let processFilesPath = process.cwd() + "/data.import/process.files.js";
-	let inputDir = process.cwd() + "/../input/all/dm_spl_monthly_update_jan2016/homeopathic"; // "/../input/selected";
-
-	let child = child_process.fork(processFilesPath, [inputDir]);
+	let child = child_process.fork(process_files_jsPath, [drugsInputDir]);
 
 	child.on("message", function (message) {
 		res.json({ success: message.success });
+
+		if (message.success) { console.log("starting pig... " + join_substances_drugs_pigPath);
+			let child2 = child_process.spawn(pigPath, ["-x", "local", join_substances_drugs_pigPath]);
+			child2.stdout.pipe(process.stdout);
+
+			// child2.stdout.on('data', function(data){
+			// 	//console.log('stdout : ' + data);
+			// });
+			//
+			// child2.stderr.on('data', function (data){
+			//  	console.log('stderr : ' + data + " process Home : " + process.env.HOME);
+			// });
+
+			// child_process.exec("pig -x local " + join_substances_drugs_pigPath, function (err, stdout, stderr) {
+			// 	console.log(stdout);
+			// });
+		}
 	});
-
-	// processFiles("/home/zoliqa/Documents/drugsdb/input/selected");
-
-	// var spawn = child_process.spawn;
-	// var pig = spawn("/home/zoliqa/Downloads/pig/pig-0.15.0/bin/pig", ["-x", "local", process.argv[2]]);
-	//
-	// pig.stdout.pipe(process.stdout);
 });
 
 module.exports = router;
