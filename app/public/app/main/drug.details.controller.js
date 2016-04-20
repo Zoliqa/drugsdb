920dd4a0b649e8c5a0b5888efc62ae61d91e65a5
Zoliqa <a_zzoli@yahoo.com>
2016-04-20 23:03:42 +0300

    populated dialog with keywords

--- app/public/app/main/drug.details.controller.js
+++ app/public/app/main/drug.details.controller.js
@@ -1,13 +1,42 @@
 define([], function () {
 
-	function DrugDetailsController($uibModalInstance, drug) {
+	function DrugDetailsController($uibModalInstance, _, drug) {
 		var vm = this;
 
 		this.drug = drug;
 		this.close = close;
+		this.getTypeDescriptions = getTypeDescriptions;
 
 		function close() {
 			$uibModalInstance.dismiss();
+		}
+
+		function getTypeDescriptions(semTypes) {
+			var descriptions = _.reduce(semTypes, function (memo, semType) {
+				var description;
+
+				switch (semType) {
+					case "sosy":
+						description = "sign or symptom";
+						break;
+
+					case "dsyn":
+						description = "disease or syndrome";
+						break;
+
+					case "orch":
+						description = "organic chemical";
+						break;
+
+					case "phsu":
+						description = "pharmacologic substance";
+						break;
+				}
+
+				return memo + (memo && ", " || "") + description;
+			}, "");
+
+			return descriptions;
 		}
 	}
 
